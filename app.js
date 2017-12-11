c577bb6b73dbd03fca5405572ac1d40ef752b5a0
ajdepersio <ajdepersio@gmail.com>
2017-12-10 20:52:00 -0500

    Getting random image

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/random', (req, res) => {
     fs.readdir('./images/', (err, files) => {
         var imgCount = files.length;
-        //var index = Math.random() * (imgCount - 0) + 0;
-        //var index = Math.random(0, imgCount);
-        var index = 1;
+        var index = Math.floor(Math.random() * (imgCount -0) + 0);
 
         var img = files[index];
         res.sendFile('/!_Repos/placecats/images/' + img);
